7779c0073bf88d84d639e25843f6ad7139ed8eb5
Isaac <isaacseligcoding@gmail.com>
2021-04-23 16:16:43 +0100

    added aboutus

---
+++ src/components/AboutUs.js
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios";
+
+class About extends Component {
+  constructor() {
+    <div>
+      <h1>About Us</h1>
+    </div>;
+  }
+}
+
+export default About;
