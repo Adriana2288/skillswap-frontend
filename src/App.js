2e866326c71ce90fae2f4097bd0524ded03d8c79
Isaac <isaacseligcoding@gmail.com>
2021-04-23 17:26:33 +0100

    homepage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import SignUp from "./components/SignUp";
 import About from "./components/AboutUs";
+import Homepage from "./components/Homepage";
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         <div>
           <Route path="/signup" component={SignUp} />
           <Route path="/aboutus" component={About} />
+          <Route path="/homepage" component={Homepage} />
         </div>
       </Router>
     );
