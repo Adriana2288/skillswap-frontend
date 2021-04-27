5318753eaf3842279920863a9696ef76d09540cd
Gloria nazer <glorianaa@hotmail.co.uk>
2021-04-28 00:24:57 +0100

    nav added and responsive

--- src/App.js
+++ src/App.js
@@ -1,30 +1,103 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import React, { Component } from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import SignUp from "./components/SignUp";
-<<<<<<< HEAD
 import LogIn from "./components/LogIn"
-=======
-import About from "./components/AboutUs";
+import AboutUs from "./components/AboutUs";
 import Landing from "./components/LandingPage";
->>>>>>> ecd476505c17768269aaa51ae99fbfe641e5e0fe
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-<<<<<<< HEAD
-          <Route path="/api/signup" component={SignUp} />
-          <Route path="/api/login" component={LogIn} />
-=======
-          <Route path="/signup" component={SignUp} />
-          <Route path="/aboutus" component={About} />
-          <Route path="/LandingPage" component={Landing} />
->>>>>>> ecd476505c17768269aaa51ae99fbfe641e5e0fe
-        </div>
-      </Router>
-    );
+// class App extends Component {
+//   render() {
+//     return (
+//       <Router>
+//         <div>
+          // <Route path="/api/SignUp" component={SignUp} />
+          // <Route path="/api/login" component={LogIn} />
+          // <Route path="/aboutus" component={About} />
+          // <Route path="/LandingPage" component={Landing} />
+//         </div>
+//         <h1>hi</h1>
+//       </Router>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+import React, { useRef, useEffect } from 'react';
+import { BrowserRouter, NavLink, Switch, Route, useLocation } from 'react-router-dom';
+import './App.css';
+
+function App() {
+
+  const navbarLinks = useRef(null);
+  const handleNavbarButton = (e) => {
+    navbarLinks.current.classList.toggle('menu-collapse');
+  };
+
+  const hideNavMenu = () => {
+    if (!navbarLinks.current.classList.contains('menu-collapse')) {
+      navbarLinks.current.classList.add('menu-collapse');
+    }
   }
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <nav className='navbar'>
+          <div className='navbar-container'>
+            <a href="#" className='brand-title'>SkillSwap</a>
+            <button onClick={(e) => { handleNavbarButton(e); }} className='navbar-toggler'>
+              <span className='navbar-toggler-icon'></span>
+            </button>
+            <div ref={navbarLinks} className='navbar-links menu-collapse'>
+              <ul className='links-list'>
+                <li className='nav-item'>
+                  <NavLink activeClassName='is-active' exact={true} className='nav-link' to='/Landing'>Landing</NavLink>
+                </li>
+                <li className='nav-item'>
+                  <NavLink activeClassName='is-active' exact={true} className='nav-link' to='/AboutUs'>About</NavLink>
+                </li>
+                <li className='nav-item'>
+                  <NavLink activeClassName='is-active' exact={true} className='nav-link' to='/SignUp'>SignUp</NavLink>
+                </li>
+                <li className='nav-item'>
+                  <NavLink activeClassName='is-active' exact={true} className='nav-link' to='/LogIn'>Login</NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className='container'>
+          <AllRoutes hideMenu={() => { hideNavMenu(); }}></AllRoutes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
+
+function AllRoutes({ hideMenu }) {
+
+  let location = useLocation();
+  useEffect(() => {
+    hideMenu();
+  }, [location]);
+
+  return (
+    <Switch>
+      <Route path="/About" component={AboutUs}>
+      </Route>
+      <Route path="/SignUp" component={SignUp}>
+      </Route>
+      <Route path="/Landing" component={Landing}>
+      </Route>
+      <Route path="/LogIn" component={LogIn}>
+      </Route>
+    </Switch>
+  );
+}
+
+
+
 
 export default App;
--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react"; 
 import axios from "axios";
 
-class About extends Component {
+class AboutUs extends Component {
   constructor() {
     <div>
-      <h1>About Us</h1>
+      <h1 className='title'> About SkillSwap </h1>
     </div>;
-    <div>
-      <p> ..........</p>
+    <div className='paragraph'>
+      <p>SkillSwap enables its users to match one another based on a specific set of skills they want to learn. Our algorithm will match 2 profiles based on their skills and interests and provides the opportunity to trade your time with others. Curious about playing the piano? Perhaps you could exchange an hour with someone in exchange for teaching them one of your skills!
+Typography
+</p>
     </div>
   }
 }
 
-export default About;
+export default AboutUs;
