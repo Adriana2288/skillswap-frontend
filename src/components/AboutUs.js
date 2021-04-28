76cdef21f15b891ff4316e52ddd9382a7a92a566
Gloria nazer <glorianaa@hotmail.co.uk>
2021-04-28 13:57:27 +0100

    no change

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"; 
+import React, { Component } from "react";
 import axios from "axios";
 
 class AboutUs extends Component {
@@ -6,10 +6,12 @@ class AboutUs extends Component {
     <div>
       <h1 className='title'> About SkillSwap </h1>
     </div>;
+
     <div className='paragraph'>
       <p>SkillSwap enables its users to match one another based on a specific set of skills they want to learn. Our algorithm will match 2 profiles based on their skills and interests and provides the opportunity to trade your time with others. Curious about playing the piano? Perhaps you could exchange an hour with someone in exchange for teaching them one of your skills!
 Typography
 </p>
+
     </div>
   }
 }
